0b533d869ab2d6e7c096803becc9abe738c9297a
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-10-21 22:11:40 -0400

    added get route paths for the stats and excercise html files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path")
 
 const PORT = process.env.PORT || 3000;
 
@@ -41,6 +42,13 @@ app.get("/api/config", (req, res) => {
 });
 
 app.use(controller);
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"))
+})
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"))
+})
 
 // Start the server
 app.listen(PORT, () => {
