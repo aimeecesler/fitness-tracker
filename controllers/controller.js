cf573fd7413fa795ac1d69b320f0b1ed2c614968
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-10-21 22:50:52 -0400

    added post route to create a new workout

--- controllers/controller.js
+++ controllers/controller.js
@@ -18,6 +18,19 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body).then((newWorkout) => {
+    res.json(newWorkout);
+  }).catch((err) => {
+    console.log(err);
+    res.json({
+      error: true,
+      data: null,
+      message: "Failed to add new workout.",
+    });
+  })
+});
+
 router.put("/api/workouts/:id", (req, res) => {
   db.Workout.findByIdAndUpdate(req.params.id, {
     $push: { exercises: req.body },
