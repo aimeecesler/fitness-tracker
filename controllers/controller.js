345aa241ee6c8a9debb83db10c33beadabaed14e
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-10-21 22:55:35 -0400

    added get route for workouts in range

--- controllers/controller.js
+++ controllers/controller.js
@@ -18,17 +18,34 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-router.post("/api/workouts", (req, res) => {
-  db.Workout.create(req.body).then((newWorkout) => {
-    res.json(newWorkout);
-  }).catch((err) => {
-    console.log(err);
-    res.json({
-      error: true,
-      data: null,
-      message: "Failed to add new workout.",
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find()
+    .then((workoutsInRange) => {
+      res.json(workoutsInRange);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to retrieve workouts in range.",
+      });
     });
-  })
+});
+
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body)
+    .then((newWorkout) => {
+      res.json(newWorkout);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to add new workout.",
+      });
+    });
 });
 
 router.put("/api/workouts/:id", (req, res) => {
